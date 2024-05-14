6ac22f475f08ac3654a1d04e15f1818d1d8ade96
Ahmed Saeed <40553471+AhmedSaeed98@users.noreply.github.com>
2024-05-14 14:28:10 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is a simple Bash test script
+
+echo "Hello, World."
+echo "Current directory: $(pwd)"
+echo "List of files in the current directory:"
+ls
+echo "Script executed successfully."
+echo "This is Ahmed Saeed the best DevOps Engineer ! - currently Senior Opearation @ Vodafone"
